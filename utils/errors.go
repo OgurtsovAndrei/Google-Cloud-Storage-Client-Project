8507236d5cf7eeaadfcf7ba8d31757fccbb73c13
Ogurtsov Andrei <ogurcovwm@gmail.com>
2025-01-25 10:38:37 +0200

    [bugfix] Fix wrong cancel connection reason
    Before we sometimes sent ContexCanceled response, which actually is consequence of error, not the original reason

--- utils/errors.go
+++ utils/errors.go
@@ -19,6 +19,8 @@ const (
 	TagNotFound        = "not-found"        // Resource not found.
 	TagInternal        = "internal-error"   // Internal server or system error, same as Panic
 	TagLogOnly         = "log-only"         // Error should only be logged, no further action.
+	TagEOF             = "eof"              // End of stream reached.
+	TagIOError         = "io-error"         // IO-related error.
 )
 
 // Writers Module (UnreliableProxyWriter) Error Codes
@@ -35,6 +37,9 @@ const (
 const (
 	ErrCodeResponseChannelNotFound = "CONN001" // Response channel not found.
 	ErrCodeHandleConnectionFailed  = "CONN002" // Connection failed to establish or maintain.
+	DataReadFailedError            = "CONN003" // Failed to read data from connection.
+	EndOfStreamError               = "CONN004" // End of stream encountered.
+	HeaderReadFailedError          = "CONN005" // Failed to read header from connection.
 )
 
 const (
